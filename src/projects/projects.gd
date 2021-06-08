64446961822f22d94b13b717c2f6a2c5f029f720
ClarkThyLord <ccpotential@gmail.com>
2021-06-07 22:01:49 -0700

    Load default preset
    Clark was here :V

--- src/projects/projects.gd
+++ src/projects/projects.gd
@@ -3,25 +3,43 @@ extends Node
 
 
 
+## Constants
+const PRESETS_DIR := "res://src/projects/presets/"
+
+
+
 ## Private Variables
-var _projects := []
+var _project : Spatial = null
+
+
+
+## Built-In Virtual Methods
+func _ready() -> void:
+	open_project_from(PRESETS_DIR + "default.tscn")
 
 
 
 ## Public Methods
-func current_project() -> String:
-	return ""
+func get_presets() -> Array:
+	var presets := []
+	var presets_dir = Directory.new()
+	if presets_dir.open(PRESETS_DIR) == OK:
+		presets_dir.list_dir_begin()
+		var file_name = presets_dir.get_next()
+		while file_name != "":
+			if not presets_dir.current_is_dir():
+				presets.append(file_name.get_basename())
+			file_name = presets_dir.get_next()
+	else:
+		print("An error occurred when trying to access project presets...")
+	return presets
 
 
-func get_project(project_name : String):
+func get_project():
 	return
 
 
-func get_projects() -> Array:
-	return []
-
-
-func new_project(project_name : String) -> void:
+func new_project() -> void:
 	pass
 
 
@@ -30,12 +48,17 @@ func save_project(project_name : String) -> int:
 
 
 func open_project(project) -> int:
+	if is_instance_valid(_project):
+		remove_child(_project)
+		_project.queue_free()
+	_project = project
+	add_child(_project)
 	return OK
 
 
 func open_project_from(project_path : String) -> int:
-	return OK
+	return open_project(load(project_path).instance())
 
 
-func close_project(project_name : String) -> void:
+func close_project() -> void:
 	pass
