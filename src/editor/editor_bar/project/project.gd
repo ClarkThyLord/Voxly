0ee1ae6769026a9bb93db0364dc2dcad2f4920c3
ClarkThyLord <ccpotential@gmail.com>
2021-06-08 22:02:40 -0700

    Add Project button
    Clark was here :V

---
+++ src/editor/editor_bar/project/project.gd
@@ -0,0 +1,29 @@
+extends MenuButton
+
+
+
+## OnReady Variables
+onready var options : PopupMenu = get_popup()
+
+onready var recents : PopupMenu = preload("res://src/editor/editor_bar/project/recents/recents.tscn").instance()
+
+
+
+## Built-In Virtual Methods
+func _ready() -> void:
+	options.add_child(recents)
+
+
+
+## Private Methods
+func _on_about_to_show():
+	options.clear()
+	
+	options.add_item("New project...", 0)
+	options.add_item("Load project...", 1)
+	options.add_submenu_item("Recent projects...", "Recents", 2)
+#	options.set_item_disabled(2, true)
+	
+	options.add_separator()
+	options.add_item("Save project", 3)
+	options.add_item("Save project as...", 4)
