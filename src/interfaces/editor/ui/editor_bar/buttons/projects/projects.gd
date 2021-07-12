4aa26c834b40a9ae477123358b01a2afb679a769
ClarkThyLord <ccpotential@gmail.com>
2021-07-11 20:51:05 -0700

    Project file dialogs
    Clark was here :V

--- src/interfaces/editor/ui/editor_bar/buttons/projects/projects.gd
+++ src/interfaces/editor/ui/editor_bar/buttons/projects/projects.gd
@@ -7,6 +7,10 @@ onready var options : PopupMenu = get_popup()
 
 onready var recents : PopupMenu = preload("res://src/interfaces/editor/ui/editor_bar/buttons/projects/recents/recents.tscn").instance()
 
+onready var save_project_dialog : FileDialog = get_node("Save")
+
+onready var open_project_dialog : FileDialog = get_node("Open")
+
 
 
 ## Built-In Virtual Methods
@@ -35,3 +39,11 @@ func _on_index_pressed(index : int):
 	match index:
 		0:
 			get_node("/root/VoxlyProjects").new_project()
+		1:
+			open_project_dialog.popup_centered_minsize()
+		2:
+			pass
+		4:
+			save_project_dialog.popup_centered_minsize()
+		5:
+			save_project_dialog.popup_centered_minsize()
