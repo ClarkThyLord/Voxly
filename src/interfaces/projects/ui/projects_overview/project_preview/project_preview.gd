1f9c1ddd74e91c4b8ffd029c926349f80e503226
ClarkThyLord <ccpotential@gmail.com>
2021-08-10 20:59:06 -0700

    List preset and recent projects
    Clark was here :V

---
+++ src/interfaces/projects/ui/projects_overview/project_preview/project_preview.gd
@@ -0,0 +1,15 @@
+extends HBoxContainer
+## Project Preview
+
+
+
+## Public Variables
+export var project_path := "" setget set_project_path
+
+
+
+## Public Methods
+func set_project_path(value : String) -> void:
+	if value.is_abs_path():
+		project_path = value
+		get_node("Name").text = project_path.get_file()
--- src/interfaces/projects/ui/projects_overview/projects_overview.gd
+++ src/interfaces/projects/ui/projects_overview/projects_overview.gd
@@ -3,6 +3,18 @@ extends PanelContainer
 
 
 
+## Constants
+const project_preview := preload("res://src/interfaces/projects/ui/projects_overview/project_preview/project_preview.tscn")
+
+
+
+## OnReady Variables
+onready var preset_projects := get_node("VBoxContainer/VBoxContainer/Presets/VBoxContainer")
+
+onready var recent_projects := get_node("VBoxContainer/VBoxContainer/Projects/VBoxContainer")
+
+
+
 ## Built-In Virtual Methods
 func _ready():
 	visible = false
@@ -10,6 +22,24 @@ func _ready():
 
 
 ## Private Methods
+func _on_visibility_changed():
+	for child in preset_projects.get_children():
+		preset_projects.remove_child(child)
+		child.queue_free()
+	for project_path in get_node("/root/VoxlyProjects").get_presets():
+		var _project_preview := project_preview.instance()
+		_project_preview.project_path = project_path
+		preset_projects.add_child(_project_preview)
+	
+	for child in recent_projects.get_children():
+		recent_projects.remove_child(child)
+		child.queue_free()
+	for project_path in get_node("/root/VoxlyProjects").get_recent_projects():
+		var _project_preview := project_preview.instance()
+		_project_preview.project_path = project_path
+		recent_projects.add_child(_project_preview)
+
+
 func _on_Load_pressed():
 	get_node("/root/VoxlyProjects").show_projects_load_dialog()
 
