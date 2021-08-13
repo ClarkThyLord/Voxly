04ddd5262f7544f15aa00a14d7405240a625aa30
ClarkThyLord <ccpotential@gmail.com>
2021-08-12 22:10:57 -0700

    Preview project name
    Clark was here :V

--- src/interfaces/projects/ui/projects_overview/project_preview/project_preview.gd
+++ src/interfaces/projects/ui/projects_overview/project_preview/project_preview.gd
@@ -12,4 +12,5 @@ export var project_path := "" setget set_project_path
 func set_project_path(value : String) -> void:
 	if value.is_abs_path():
 		project_path = value
-		get_node("Name").text = project_path.get_file()
+		get_node("Name").text = project_path.get_file()\
+				.replace("." + project_path.get_extension(), "").capitalize()
