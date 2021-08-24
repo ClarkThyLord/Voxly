eccead935ff14ae0a715ed4ed7fc42c1a7599f50
ClarkThyLord <ccpotential@gmail.com>
2021-08-23 22:46:47 -0700

    Update projects_menu.gd
    Clark was here :V

--- src/interfaces/editor/ui/editor_bar/menus/projects_menu/projects_menu.gd
+++ src/interfaces/editor/ui/editor_bar/menus/projects_menu/projects_menu.gd
@@ -14,7 +14,7 @@ onready var recents_menu : PopupMenu = preload("res://src/interfaces/editor/ui/e
 func _ready() -> void:
 	options.add_child(recents_menu)
 	
-	get_popup().connect("index_pressed", self, "_on_index_pressed")
+	get_popup().connect("id_pressed", self, "_on_id_pressed")
 
 
 
@@ -25,28 +25,31 @@ func _on_about_to_show():
 	
 	options.clear()
 	
-	options.add_item("New project...")
-	options.add_item("Load project...")
-	options.add_submenu_item("Recent projects...", "RecentsMenu")
+	options.add_item("New project...", 0)
+	options.add_item("Load project...", 1)
+	options.add_item("Open project...", 2)
+	options.add_submenu_item("Recent projects...", "RecentsMenu", 3)
 	if recents_menu.get_item_count() == 0:
 		options.set_item_disabled(options.get_item_count() - 1, true)
 	
 	options.add_separator()
-	options.add_item("Save project")
+	options.add_item("Save project", 4)
 	if not get_node("/root/VoxlyProjects").get_project_path():
 		options.set_item_disabled(options.get_item_count() - 1, true)
-	options.add_item("Save project as...")
+	options.add_item("Save project as...", 5)
 	if not is_instance_valid(get_node("/root/VoxlyProjects").get_project()):
 		options.set_item_disabled(options.get_item_count() - 2, true)
 		options.set_item_disabled(options.get_item_count() - 1, true)
 
 
-func _on_index_pressed(index : int):
+func _on_id_pressed(index : int):
 	match index:
 		0:
-			get_node("/root/VoxlyProjects").show_projects_overview()
+			get_node("/root/VoxlyProjects").new_project()
 		1:
 			get_node("/root/VoxlyProjects").show_projects_load_dialog()
+		2:
+			get_node("/root/VoxlyProjects").show_projects_overview()
 		4:
 			get_node("/root/VoxlyProjects").save_project()
 		5:
