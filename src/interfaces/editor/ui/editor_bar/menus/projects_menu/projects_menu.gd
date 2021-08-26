3b1b23c7b0fe7cd6d05f708ef2201af37a622da8
ClarkThyLord <ccpotential@gmail.com>
2021-08-25 21:22:09 -0700

    Improve projects menu & save as preset
    Clark was here :V

--- src/interfaces/editor/ui/editor_bar/menus/projects_menu/projects_menu.gd
+++ src/interfaces/editor/ui/editor_bar/menus/projects_menu/projects_menu.gd
@@ -25,9 +25,11 @@ func _on_about_to_show():
 	
 	options.clear()
 	
+	options.add_item("View projects", 2)
+	
+	options.add_separator()
 	options.add_item("New project...", 0)
-	options.add_item("Load project...", 1)
-	options.add_item("Open project...", 2)
+	options.add_item("Open project...", 1)
 	options.add_submenu_item("Recent projects...", "RecentsMenu", 3)
 	if recents_menu.get_item_count() == 0:
 		options.set_item_disabled(options.get_item_count() - 1, true)
@@ -37,7 +39,9 @@ func _on_about_to_show():
 	if not get_node("/root/VoxlyProjects").get_project_path():
 		options.set_item_disabled(options.get_item_count() - 1, true)
 	options.add_item("Save project as...", 5)
+	options.add_item("Save as preset...", 6)
 	if not is_instance_valid(get_node("/root/VoxlyProjects").get_project()):
+		options.set_item_disabled(options.get_item_count() - 3, true)
 		options.set_item_disabled(options.get_item_count() - 2, true)
 		options.set_item_disabled(options.get_item_count() - 1, true)
 
@@ -54,3 +58,7 @@ func _on_id_pressed(index : int):
 			get_node("/root/VoxlyProjects").save_project()
 		5:
 			get_node("/root/VoxlyProjects").show_projects_save_dialog()
+		6:
+			get_node("/root/VoxlyProjects").add_preset(
+					get_node("/root/VoxlyProjects").get_project_path().get_file(),
+					get_node("/root/VoxlyProjects").get_project())
