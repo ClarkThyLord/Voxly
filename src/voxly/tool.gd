b1636c6ff4a4d2347dac31913b048a8d4c82729c
ClarkThyLord <ccpotential@gmail.com>
2021-09-18 23:11:32 -0700

    Working on VoxlyTool
    Clark was here :V

---
+++ src/voxly/tool.gd
@@ -0,0 +1,22 @@
+extends Reference
+class_name VoxlyTool
+## Voxly Tool Class
+
+
+
+## Public Variables
+var name := ""
+
+
+
+## Public Methods
+func selected() -> void:
+	return
+
+
+func unselected() -> void:
+	return
+
+
+func use(camera : Camera, event : InputEvent) -> void:
+	pass
