df9b13a036d79479ebe70446fc109e0b4e98558a
ClarkThyLord <ccpotential@gmail.com>
2021-09-21 22:51:58 -0700

    Make VoxlyTools inherit BaseButton
    Clark was here :V

--- src/voxly/tool.gd
+++ src/voxly/tool.gd
@@ -1,22 +1,9 @@
-extends Reference
+extends BaseButton
 class_name VoxlyTool
 ## Voxly Tool Class
 
 
 
-## Public Variables
-var name := ""
-
-
-
 ## Public Methods
-func selected() -> void:
-	return
-
-
-func unselected() -> void:
-	return
-
-
 func use(camera : Camera, event : InputEvent) -> void:
 	pass
