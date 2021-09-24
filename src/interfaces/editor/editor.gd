2519a477f98690f7bef2ee0759c36ed5e48a1692
ClarkThyLord <ccpotential@gmail.com>
2021-09-23 22:34:09 -0700

    Use BaseButton type
    Clark was here :V

--- src/interfaces/editor/editor.gd
+++ src/interfaces/editor/editor.gd
@@ -69,42 +69,42 @@ func _ready() -> void:
 
 ## Public Methods
 func add_button_to_editor_bar_left(
-		button : Button) -> void:
+		button : BaseButton) -> void:
 	ui.editor_bar_left.add_child(button)
 
 
 func remove_button_from_editor_bar_left(
-		button : Button) -> void:
+		button : BaseButton) -> void:
 	ui.editor_bar_left.remove_child(button)
 
 
 func add_button_to_editor_bar_center(
-		button : Button) -> void:
+		button : BaseButton) -> void:
 	ui.editor_bar_center.add_child(button)
 
 
 func remove_button_from_editor_bar_center(
-		button : Button) -> void:
+		button : BaseButton) -> void:
 	ui.editor_bar_center.remove_child(button)
 
 
 func add_button_to_editor_bar_right(
-		button : Button) -> void:
+		button : BaseButton) -> void:
 	ui.editor_bar_right.add_child(button)
 
 
 func remove_button_from_editor_bar_right(
-		button : Button) -> void:
+		button : BaseButton) -> void:
 	ui.editor_bar_right.remove_child(button)
 
 
 func add_button_to_editor_tools(
-		button : Button) -> void:
+		button : BaseButton) -> void:
 	ui.editor_tools.add_child(button)
 
 
 func remove_button_from_editor_tools(
-		button : Button) -> void:
+		button : BaseButton) -> void:
 	ui.editor_tools.remove_child(button)
 
 
