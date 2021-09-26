e2ca595ac8da6386d397ed5d652d83b35452eb54
ClarkThyLord <ccpotential@gmail.com>
2021-09-25 23:43:31 -0700

    Rework VoxlyTool
    Clark was here :V

--- src/voxly/tool.gd
+++ src/voxly/tool.gd
@@ -1,9 +1,24 @@
-extends BaseButton
+extends Reference
 class_name VoxlyTool
 ## Voxly Tool Class
 
 
 
+## Public Variables
+var name := ""
+
+var can_select_single := true
+
+var can_select_multiple := false
+
+var can_mirror_x := false
+
+var can_mirror_y := false
+
+var can_mirror_z := false
+
+
+
 ## Public Methods
-func use(camera : Camera, event : InputEvent) -> void:
+func use(selected : Dictionary) -> void:
 	pass
